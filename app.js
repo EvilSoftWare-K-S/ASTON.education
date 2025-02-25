b017981f9df2cfab83e6ed161125ee804a9b9364
EvilSoftWare-K-S <sergey.krahmaliov@gmail.com>
2025-02-25 17:45:53 +0400

    Added fruits Basket array. Implemented the getRandomNumber function. The strWriteFruit function now outputs a random fruit from the fruit Basket array instead of the fixed value banana.

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+let fruitBasket = ["banan.","apple.","pineaplle.","apricot.","kiwi."];
+
+let getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 let strWriteFruit = () => {
-  console.log("banan.");
+  let random = getRandomNumber(0,fruitBasket.length - 1);
+  console.log(fruitBasket[random]);
 };
 
 strWriteFruit();
